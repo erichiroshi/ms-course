e4777c0f8f832d144f6685120645b4253b5cdcff
erichiroshi <erichiroshi@gmail.com>
2020-12-28 10:36:51 -0400

    Login, JWT Token

--- hr-oauth/src/main/java/com/erichiroshi/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/erichiroshi/hroauth/services/UserService.java
@@ -3,6 +3,9 @@ package com.erichiroshi.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.erichiroshi.hroauth.feignclients.UserFeignClient;
@@ -10,7 +13,7 @@ import com.erichiroshi.hroauth.feignclients.UserFeignClient;
 import entities.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -18,13 +21,23 @@ public class UserService {
 	private UserFeignClient userFeignClient;
 
 	public User findByEmail(String email) {
-
 		User user = userFeignClient.findByEmail(email).getBody();
 		if (user == null) {
 			logger.error("Email not found: " + email);
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 

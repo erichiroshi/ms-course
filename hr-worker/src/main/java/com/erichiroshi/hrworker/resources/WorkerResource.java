9f4e5d1028ba0c4e24fffcd96ab026f34557a932
erichiroshi <erichiroshi@gmail.com>
2023-03-07 14:14:18 -0400

    Zuul timeout

--- hr-worker/src/main/java/com/erichiroshi/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/erichiroshi/hrworker/resources/WorkerResource.java
@@ -35,6 +35,12 @@ public class WorkerResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
 		logger.info("PORT = " + environment.getProperty("local.server.port"));
 
 		return ResponseEntity.ok(repository.findById(id).orElseThrow());

f5fa3883cde3d8502ad8ac939a051e6b695886dd
erichiroshi <erichiroshi@gmail.com>
2023-03-07 08:28:11 -0400

    Implementando projeto hr-worker

---
+++ hr-worker/src/main/java/com/erichiroshi/hrworker/entities/Worker.java
@@ -0,0 +1,33 @@
+package com.erichiroshi.hrworker.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EqualsAndHashCode.Include
+	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private Double dailyIncome;
+}
